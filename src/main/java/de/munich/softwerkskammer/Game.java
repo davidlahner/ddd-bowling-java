f8a11528acca42d47e3c47eba3986966d0d07bf9
Yves Thrier <yves.thrier@zuehlke.com>
2015-08-19 16:06:40 +0200

    Rolling one should return one

--- src/main/java/de/munich/softwerkskammer/Game.java
+++ src/main/java/de/munich/softwerkskammer/Game.java
@@ -1,49 +1,57 @@
 package de.munich.softwerkskammer;
 
 public class Game {
-    private int rolls[] = new int[21];
-    private int currentRoll = 0;
+	private static final int NUMBER_OF_FRAMES = 10;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
 
-    public void roll(int pins) {
-        rolls[currentRoll++] = pins;
-    }
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+	}
 
-    public int score() {
-        int score = 0;
-        int frameIndex = 0;
-        for (int frame = 0; frame < 10; frame++) {
-            if (isStrike(frameIndex)) {
-                score += 10 + strikeBonus(frameIndex);
-                frameIndex++;
-            } else if (isSpare(frameIndex)) {
-                score += 10 + spareBonus(frameIndex);
-                frameIndex += 2;
-            } else {
-                score += sumOfBallsInFrame(frameIndex);
-                frameIndex += 2;
-            }
-        }
-        return score;
-    }
+	public int score(int frame) {
+		return runningScore(frame);
+	}
 
-    private boolean isStrike(int frameIndex) {
-        return rolls[frameIndex] == 10;
-    }
+	public int score() {
+		return runningScore(NUMBER_OF_FRAMES);
+	}
 
-    private boolean isSpare(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
-    }
+	private int runningScore(int untilFrame) {
+		int score = 0;
+		int frameIndex = 0;
+		for (int frame = 0; frame < untilFrame; frame++) {
+			if (isStrike(frameIndex)) {
+				score += 10 + strikeBonus(frameIndex);
+				frameIndex++;
+			} else if (isSpare(frameIndex)) {
+				score += 10 + spareBonus(frameIndex);
+				frameIndex += 2;
+			} else {
+				score += sumOfBallsInFrame(frameIndex);
+				frameIndex += 2;
+			}
+		}
+		return score;
+	}
 
-    private int strikeBonus(int frameIndex) {
-        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
-    }
+	private boolean isStrike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
 
-    private int spareBonus(int frameIndex) {
-        return rolls[frameIndex + 2];
-    }
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+	}
 
-    private int sumOfBallsInFrame(int frameIndex) {
-        return rolls[frameIndex] + rolls[frameIndex + 1];
-    }
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+	}
 
+	private int spareBonus(int frameIndex) {
+		return rolls[frameIndex + 2];
+	}
+
+	private int sumOfBallsInFrame(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1];
+	}
 }
--- src/test/java/de/munich/softwerkskammer/GameTest.java
+++ src/test/java/de/munich/softwerkskammer/GameTest.java
@@ -1,125 +1,137 @@
 package de.munich.softwerkskammer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
-    private Game game;
+	private Game game;
 
-    @Before
-    public void beforeTest() {
-        game = new Game();
-    }
+	@Before
+	public void beforeTest() {
+		game = new Game();
+	}
 
-    @Test
-    public void initialScoreShouldBeZero() {
-        assertEquals(0, game.score());
-    }
+	@Test
+	public void rollingOneShouldReturnOne() {
+		// Arrange
+		game.roll(1);
 
-    @Test
-    public void strikesOnlyShouldReturn300() {
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        game.roll(10);
-        assertEquals(300, game.score());
-    }
+		// Act
+		int score = game.score(1);
 
-    @Test
-    public void pairsOfNineAndMissShouldReturn90() {
-        game.roll(9);
-        game.roll(0);
+		// Assert
+		assertEquals(1, score);
+	}
 
-        game.roll(9);
-        game.roll(0);
+	@Test
+	public void initialScoreShouldBeZero() {
+		assertEquals(0, game.score());
+	}
 
-        game.roll(9);
-        game.roll(0);
+	@Test
+	public void strikesOnlyShouldReturn300() {
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		game.roll(10);
+		assertEquals(300, game.score());
+	}
 
-        game.roll(9);
-        game.roll(0);
+	@Test
+	public void pairsOfNineAndMissShouldReturn90() {
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
+		game.roll(9);
+		game.roll(0);
 
-        game.roll(9);
-        game.roll(0);
-        assertEquals(90, game.score());
-    }
+		game.roll(9);
+		game.roll(0);
 
-    @Test
-    public void fivesOnlyShouldReturn150() {
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        assertEquals(150, game.score());
-    }
+		game.roll(9);
+		game.roll(0);
 
-    @Test(expected = RuntimeException.class)
-    public void rollingMoreThen21TimesShouldThrowException() {
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-        game.roll(5);
-    }
+		game.roll(9);
+		game.roll(0);
+
+		game.roll(9);
+		game.roll(0);
+		assertEquals(90, game.score());
+	}
+
+	@Test
+	public void fivesOnlyShouldReturn150() {
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		assertEquals(150, game.score());
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void rollingMoreThen21TimesShouldThrowException() {
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+		game.roll(5);
+	}
 
 }
