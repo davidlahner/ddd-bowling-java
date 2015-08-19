f3609ba916488a44c69ec4fbeb39e34aca7c71af
Yves Thrier <yves.thrier@zuehlke.com>
2015-08-19 16:55:23 +0200

    Add rolls to frame instead of rolls array
    Now instead of having an int array, we pass rolls
    to the frame. We add lookup methods for the rolls

--- src/main/java/de/munich/softwerkskammer/Frame.java
+++ src/main/java/de/munich/softwerkskammer/Frame.java
@@ -1,10 +1,12 @@
 package de.munich.softwerkskammer;
 
 public class Frame {
+	private final Rolls theRolls;
 	private final int[] rolls;
 	private int firstRollOfFramePosition;
 
-	public Frame(int[] rolls, int firstRollOfFramePosition) {
+	public Frame(Rolls theRolls, int[] rolls, int firstRollOfFramePosition) {
+		this.theRolls = theRolls;
 		this.rolls = rolls;
 		this.firstRollOfFramePosition = firstRollOfFramePosition;
 	}
